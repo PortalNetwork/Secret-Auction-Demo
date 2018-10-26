89c39976fdab9000831aab1a409be0eadd851821
Phyrex Tsai <x01ep23i@hotmail.com>
2018-10-26 11:28:53 +0800

    Update index.js for more description

--- migrations/2_deploy_millionaires_problem_factory.js
+++ migrations/2_deploy_millionaires_problem_factory.js
@@ -2,7 +2,8 @@ var EnigmaCollectible = artifacts.require("./EnigmaCollectible.sol");
 var AuctionFactory = artifacts.require("./AuctionFactory.sol");
 
 module.exports = function(deployer) {
-  const enigmaAddr = "0x03F8163354A9DE09eA118372A8cFD20250ed7983";
+  // TODO: Update the enigma address
+  const enigmaAddr = "0x767d422364eC67f71C148C47DE96F09ffDd6f367";
   deployer.deploy(EnigmaCollectible, "ENG721", "E721").then(function() {
     return deployer.deploy(AuctionFactory, enigmaAddr, EnigmaCollectible.address);
   });
--- src/index.js
+++ src/index.js
@@ -25,30 +25,42 @@ const secretAuctionDemo = async() => {
 		// Initialize enigma configuration
 		let enigmaSetup = new EnigmaSetup();
 		await enigmaSetup.init();
+		console.log("");
+		console.log("======================================================");
+		console.log("");
+		console.log("Enigma Secret Auction");
+		console.log("");
+		console.log("This demo shows three user using Enigma Secret Auction");
+		console.log("to start an auction and encrypt the bid value.");
+		console.log("After the bid process finished, get the winner from ");
+		console.log("the auction contract.");
+		console.log("");
+		console.log("======================================================");
+		console.log("");
 
-		console.log("==== Setup enigma ====");
+		console.log("==== 1. Setup enigma ====");
 		const {web3, accounts} = enigmaSetup;
-		// Replace with your auctionFatcory address here
-		const auctionFactoryContractAddress = "0x151660b8D63ECBf76BE2A44f49CF995d309D400e";
+		// TODO: Replace with your auctionFatcory address here
+		const auctionFactoryContractAddress = "0xe5AE972aE181ba926E6B8760ea9992F383654f34";
 		const auctionFactory = await getContractInstance(
 			web3,
 			auctionFactoryContractDefinition,
 			auctionFactoryContractAddress
 		);
-		console.log("==== Create auction with expired time (86400) and starting price (500000) ====");
+		console.log("==== 2. Create auction with expired time (86400) and starting price (500000) ====");
 		// Create new auction with expired time(in seconds) and starting price(in wei)
-		await auctionFactory.createAuction(864000, 500000,{
+		await auctionFactory.createAuction(864000, 500000, {
 			from: accounts[0],
 			gas: GAS
 		});
 
-		console.log("==== Get auction factory address list ====");
+		console.log("==== 3. Get auction factory address list ====");
 		const addr = await auctionFactory.getAuctionAddresses({
 				from: accounts[0],
 				gas: GAS
 		});
 
-		console.log("==== Get latest auction address ====");
+		console.log("==== 4. Get latest auction address ====");
 		// Always use the latest auction address
 		const auctionAddress = addr[addr.length -1];
 		// Initialize contract instance to call contract later
@@ -58,7 +70,7 @@ const secretAuctionDemo = async() => {
 				auctionAddress
 		);
 
-		console.log("==== Send " + web3.utils.toWei("8000000", "wei") + " from " + accounts[0] + " to stake pool ====");
+		console.log("==== 5. Send " + web3.utils.toWei("8000000", "wei") + " from " + accounts[0] + " to stake pool ====");
 		// Bidders must stake tokens first which is greater than or equal to starting price  
 		await auction.stake({
 				from: accounts[0],
@@ -66,31 +78,45 @@ const secretAuctionDemo = async() => {
 				value: web3.utils.toWei("8000000","wei")
 		});
 
-		console.log("==== Send " + web3.utils.toWei("9000000", "wei") + " from " + accounts[1] + " to stake pool ====");
+		console.log("==== 6. Send " + web3.utils.toWei("9000000", "wei") + " from " + accounts[1] + " to stake pool ====");
 		await auction.stake({
 				from: accounts[1],
 				gas: GAS,
 				value: web3.utils.toWei("9000000","wei")
 		});
 
-		console.log("==== Ecrypt bidding value of " + accounts[0] + " ====");
+		console.log("==== 7. Send " + web3.utils.toWei("10000000", "wei") + " from " + accounts[2] + " to stake pool ====");
+		await auction.stake({
+				from: accounts[2],
+				gas: GAS,
+				value: web3.utils.toWei("10000000","wei")
+		});
+
 		// Bid value must be less than stake value
 		let bidVal = await getEncryptedValue("1000000");
 		await auction.bid(bidVal, {
 				from: accounts[0],
 				gas: GAS
 		});
+		console.log("==== 8. Ecrypt bidding value of 1000000 to " + bidVal + " from " + accounts[0] + " ====");
 
-		console.log("==== Ecrypt bidding value of " + accounts[1] + " ====");
-		bidVal = await getEncryptedValue("2000000");
+		bidVal = await getEncryptedValue("5000000");
 		await auction.bid(bidVal,{
 				from: accounts[1],
 				gas: GAS
 		});
+		console.log("==== 9. Ecrypt bidding value of 5000000 to " + bidVal + " from " + accounts[1] + " ====");
+
+		bidVal = await getEncryptedValue("9000000");
+		await auction.bid(bidVal,{
+				from: accounts[2],
+				gas: GAS
+		});
+		console.log("==== 10. Ecrypt bidding value of 9000000 to " + bidVal + " from " + accounts[2] + " ====");
 
 		// For preparing arguments of the highest bidder computing task
 		const bidders = await auction.getBidders();
-		console.log("==== List bidders ====");
+		console.log("==== 11. List bidders ====");
 		console.log(bidders);
 		let stakeAmounts = [];
 		let bidValue = [];
@@ -115,12 +141,13 @@ const secretAuctionDemo = async() => {
 		you run the secure computation from your front-end!!!
 		*/
 	
-		console.log("==== Create task with parameter", blockNumber,
+		console.log("==== 11. Create task with parameter");
+		/*console.log(blockNumber,
 		auctionAddress,
 		CALLABLE,
 		[bidders, bidValue, stakeAmounts],
 		CALLBACK,
-		ENG_FEE, " ====");
+		ENG_FEE, " ====");*/
 		let task = await enigmaSetup.enigma.createTask(
 			blockNumber,
 			auctionAddress,
@@ -136,18 +163,19 @@ const secretAuctionDemo = async() => {
 			gas: GAS
 		});
 
-		console.log("==== Compute by enigma ====");
+		console.log("==== 12. Compute by enigma ====");
 		let result = await task.compute({
 			from: accounts[0],
 			gas: GAS
 		});
 
-		console.log("==== After auction listen the winner ====");
+		console.log("==== 13. After auction listen the winner ====");
 		const callbackFinishedEvent = auction.Winner();
 		callbackFinishedEvent.watch(async (err, res)=>{
 			if(!err){
 				console.log('result: ', res);
-				console.log('winner: ', await auction.getWinner());
+				//console.log('bidValue: ', web3.utils.fromWei(res.args.bidValue.toNumber().toString(), "ether"));
+				console.log('==== 14. winner: ' + await auction.getWinner() + " ====");
 				callbackFinishedEvent.stopWatching((err, res)=> {
 					if(err){
 						console.log(err);
